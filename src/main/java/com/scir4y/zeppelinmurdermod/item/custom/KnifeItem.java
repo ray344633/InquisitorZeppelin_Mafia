01a90956fc1b7b4154e5313fb0264c9898a83e41
Scir4y <ratmirhamdohov35@gmail.com>
2026-07-13 20:09:15 +0300

    First upd

---
+++ src/main/java/com/scir4y/zeppelinmurdermod/item/custom/KnifeItem.java
@@ -0,0 +1,42 @@
+package com.scir4y.zeppelinmurdermod.item.custom;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.EquipmentSlotGroup;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.component.ItemAttributeModifiers;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class KnifeItem extends Item {
+    public KnifeItem(Tier iron, Properties properties) {
+        super(properties);
+    }
+
+    public static ItemAttributeModifiers createAttributes(Tier tier, int attackDamage, float attackSpeed) {
+        return createAttributes(tier, (float) attackDamage, attackSpeed);
+    }
+    public static ItemAttributeModifiers createAttributes(Tier p_330371_, float p_331976_, float p_332104_) {
+        return ItemAttributeModifiers.builder().add(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_ID, p_331976_ + p_330371_.getAttackDamageBonus(), AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND).add(Attributes.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED_ID, p_332104_, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND).build();
+    }
+
+    public boolean canAttackBlock(BlockState state, Level level, BlockPos pos, Player player) {
+        return !player.isCreative();
+    }
+
+    public boolean hurtEnemy(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        Level level = attacker.level();
+        return true;
+        }
+
+    public void postHurtEnemy(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+    }
+
+
+
+}
